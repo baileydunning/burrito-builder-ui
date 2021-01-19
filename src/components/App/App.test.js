a5b7ce68e0a28158b66898dc781863d0d914f7d5
Bailey Dunning <bwdunning@gmail.com>
2021-01-19 11:24:16 -0700

    Comment out console suggestions

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,1 +1,21 @@
+import React from 'react'
+import App from './App'
+import { act, render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import '@testing-library/jest-dom'
+import { getOrders, postOrder, deleteOrder } from '../../apiCalls'
+import { testOrders } from '../../sampleData'
+jest.mock('../../apiCalls')
 
+describe('App', () => {
+  beforeEach(async () => {
+    getOrders.mockResolvedValueOnce(() => testOrders.orders)
+    await act(async () => {
+      render(<App />)
+    })
+  })
+
+  it('should render the app', () => {
+    
+  })
+})
