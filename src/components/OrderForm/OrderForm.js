46a21f47290c0a176ffc3d0cd6fd608f63558f20
Bailey Dunning <bwdunning@gmail.com>
2021-01-19 09:56:03 -0700

    Add error handling for name input

--- src/components/OrderForm/OrderForm.js
+++ src/components/OrderForm/OrderForm.js
@@ -1,79 +1,81 @@
 import React, { useState } from 'react'
 
-  const OrderForm = ({ addOrder }) => {
-    const [name, setName] = useState('')
-    const [ingredients, setIngredients] = useState([])
+const OrderForm = ({ addOrder }) => {
+  const [name, setName] = useState('')
+  const [ingredients, setIngredients] = useState([])
 
-    const makeCheckboxes = () => {
-      const possibleIngredients = ['beans', 'steak', 'carnitas', 'sofritas', 'lettuce', 'queso fresco', 'pico de gallo', 'hot sauce', 'guacamole', 'jalapenos', 'cilantro', 'sour cream']
-      return possibleIngredients.map(ingredient => {
-        return (
-          <label htmlFor='checkbox'>
-          <input 
-            key={ingredient} 
+  const makeCheckboxes = () => {
+    const possibleIngredients = ['beans', 'steak', 'carnitas', 'sofritas', 'lettuce', 'queso fresco', 'pico de gallo', 'hot sauce', 'guacamole', 'jalapenos', 'cilantro', 'sour cream']
+    return possibleIngredients.map(ingredient => {
+      return (
+        <label htmlFor='checkbox'>
+          <input
+            key={ingredient}
             name={ingredient}
             type='checkbox'
             checked={ingredients.includes(ingredient) ? true : false}
             onChange={() => handleIngredients(ingredient)}
           />
-            {ingredient}
-          </label>
-        )
-      })
-    }
-
-    const handleIngredients = (ingredient) => {
-      if (!ingredients.includes(ingredient)) {
-        setIngredients([...ingredients, ingredient])
-      } else {
-        const filteredIngredients = ingredients.filter(item => {
-          return ingredient !== item
-        })
-        setIngredients(filteredIngredients)
-      }
-    }
-
-    const handleSubmit = e => {
-      e.preventDefault()
-
-      const burritoOrder = {
-        name: name,
-        ingredients: ingredients
-      }
-
-      if (ingredients.length === 0) {
-        alert('Add some ingredients to your order!')
-      } else {
-        addOrder(burritoOrder)
-        clearInputs()
-      }
-    }
-
-    const clearInputs = () => {
-      setName('')
-      setIngredients([])
-    }
-
-    return (
-      <form>
-        <input
-          type='text'
-          placeholder='Name'
-          name='name'
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-
-        { makeCheckboxes() }
-
-        <p>Order: {ingredients.join(', ') || 'Nothing selected'}</p>
-
-        <button onClick={e => handleSubmit(e)}>
-          Submit Order
-        </button>
-      </form>
-    )
-
+          {ingredient}
+        </label>
+      )
+    })
   }
+
+  const handleIngredients = (ingredient) => {
+    if (!ingredients.includes(ingredient)) {
+      setIngredients([...ingredients, ingredient])
+    } else {
+      const filteredIngredients = ingredients.filter(item => {
+        return ingredient !== item
+      })
+      setIngredients(filteredIngredients)
+    }
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+
+    const burritoOrder = {
+      name: name,
+      ingredients: ingredients
+    }
+
+    if (ingredients.length === 0) {
+      alert('Add some ingredients to your order!')
+    } else if (!name) {
+      alert('Please enter your name!')
+    } else {
+      addOrder(burritoOrder)
+      clearInputs()
+    }
+  }
+
+  const clearInputs = () => {
+    setName('')
+    setIngredients([])
+  }
+
+  return (
+    <form>
+      <input
+        type='text'
+        placeholder='Name'
+        name='name'
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      { makeCheckboxes()}
+
+      <p>Order: {ingredients.join(', ') || 'Nothing selected'}</p>
+
+      <button onClick={e => handleSubmit(e)}>
+        Submit Order
+        </button>
+    </form>
+  )
+
+}
 
 export default OrderForm;
